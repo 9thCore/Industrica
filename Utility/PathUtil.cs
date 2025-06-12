d20c3956cb5c5a722a4f86b33611ff612260dfc6
9thCore <38700349+9thCore@users.noreply.github.com>
2025-06-12 20:07:01 +0300

    basic utility

---
+++ Utility/PathUtil.cs
@@ -0,0 +1,25 @@
+﻿using Nautilus.Utility;
+using System.IO;
+using System.Reflection;
+
+namespace Industrica.Utility
+{
+    public static class PathUtil
+    {
+        private static string _assemblyPath;
+        public static string AssemblyPath => _assemblyPath ??= Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+        private static string _assetPath;
+        public static string AssetPath => _assetPath ??= Path.Combine(AssemblyPath, "Assets");
+
+        public static Atlas.Sprite GetImage(string path)
+        {
+            string fullPath = Path.Combine(AssetPath, $"{path}.png");
+            if (!File.Exists(fullPath))
+            {
+                throw new FileNotFoundException();
+            }
+            return ImageUtils.LoadSpriteFromFile(fullPath);
+        }
+    }
+}
