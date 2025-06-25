9cb0b702274ad6575abc500344774980960a3840
9thCore <38700349+9thCore@users.noreply.github.com>
2025-06-25 16:46:57 +0300

    Value that changes over time towards a target

---
+++ Utility/SmoothValue.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using UnityEngine;
+
+namespace Industrica.Utility
+{
+    public class SmoothValue
+    {
+        public float value;
+
+        private float start;
+        private float target;
+        private float duration;
+        private float elapsed;
+        private bool finished;
+
+        public delegate void OnFinishHandler(float value);
+        public event OnFinishHandler OnFinish;
+
+        public SmoothValue(float initialValue, float initialDuration)
+        {
+            value = initialValue;
+            start = initialValue;
+            target = initialValue;
+            duration = initialDuration;
+            elapsed = 0f;
+            finished = false;
+        }
+
+        public SmoothValue(float initialDuration) : this(0f, initialDuration)
+        {
+
+        }
+
+        public void SetTarget(float target)
+        {
+            this.target = target;
+            start = value;
+            elapsed = 0f;
+            finished = false;
+        }
+
+        public void SetDuration(float duration)
+        {
+            this.duration = duration;
+        }
+
+        public bool IsChanging()
+        {
+            return elapsed < duration;
+        }
+
+        public void Update()
+        {
+            if (elapsed >= duration)
+            {
+                if (!finished)
+                {
+                    OnFinish?.Invoke(target);
+                    finished = true;
+                }
+
+                value = target;
+                return;
+            }
+
+            float time = elapsed / duration;
+            value = Mathf.Lerp(start, target, time);
+            elapsed += DayNightCycle.main.deltaTime;
+        }
+    }
+}
