7d29901735f2b5c27f486f77f3777c5390be6a68
9thCore <38700349+9thCore@users.noreply.github.com>
2025-06-26 22:40:41 +0300

    Save entry specialised for components

---
+++ Save/ComponentSaveData.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Industrica.Save
+{
+    public abstract class ComponentSaveData<T> : PrefabIdentifiableSaveData where T : MonoBehaviour
+    {
+        public override PrefabIdentifier GetIdentifier => Component.GetComponent<PrefabIdentifier>();
+
+        [JsonIgnore]
+        public T Component { get; protected set; }
+
+        public ComponentSaveData(T component)
+        {
+            Component = component;
+
+            if (component != null)
+            {
+                LoadDataLate();
+            }
+        }
+
+        public override bool ValidForSaving()
+        {
+            return Component != null;
+        }
+    }
+}
