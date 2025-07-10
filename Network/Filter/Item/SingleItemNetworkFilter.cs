1084d7237113cb49f9e4c78b9701c4faeeb63094
9thCore <38700349+9thCore@users.noreply.github.com>
2025-07-10 15:16:14 +0300

    Start work on networks

---
+++ Network/Filter/Item/SingleItemNetworkFilter.cs
@@ -0,0 +1,12 @@
+﻿namespace Industrica.Network.Filter.Item
+{
+    public class SingleItemNetworkFilter : NetworkFilter<Pickupable>
+    {
+        public TechType techType;
+
+        public override bool Matches(Pickupable value)
+        {
+            return value.GetTechType() == techType;
+        }
+    }
+}
---
+++ Network/Filter/NetworkFilter.cs
@@ -0,0 +1,7 @@
+﻿namespace Industrica.Network.Filter
+{
+    public abstract class NetworkFilter<T>
+    {
+        public abstract bool Matches(T value);
+    }
+}
