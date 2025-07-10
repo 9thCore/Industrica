3414fdefe6624def6c8396a83a6f94d44b4ffbd4
9thCore <38700349+9thCore@users.noreply.github.com>
2025-07-10 14:30:15 +0300

    Update build.bat

--- build.bat
+++ build.bat
@@ -2,6 +2,6 @@ set mod=Industrica
 set modpath=%SUBNAUTICA_PATH%\BepInEx\plugins\%mod%
 mkdir "%modpath%"
 copy "bin\Release\net472\%mod%.dll" "%modpath%\%mod%.dll"
+copy "bin\Release\net472\%mod%.pdb" "%modpath%\%mod%.pdb"
 robocopy "Localization" "%modpath%\Localization" /E
 robocopy "Assets" "%modpath%\Assets" /E
-exit /B 0
