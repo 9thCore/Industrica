d8690b09ee763059bca30dcd84a9d4c6feb8f5b4
9thCore <38700349+9thCore@users.noreply.github.com>
2025-07-10 17:03:13 +0300

    Update build.bat

--- build.bat
+++ build.bat
@@ -3,5 +3,5 @@ set modpath=%SUBNAUTICA_PATH%\BepInEx\plugins\%mod%
 mkdir "%modpath%"
 copy "bin\Release\net472\%mod%.dll" "%modpath%\%mod%.dll"
 copy "bin\Release\net472\%mod%.pdb" "%modpath%\%mod%.pdb"
-robocopy "Localization" "%modpath%\Localization" /E
-robocopy "Assets" "%modpath%\Assets" /E
+(robocopy "Localization" "%modpath%\Localization" /E) ^& IF %ERRORLEVEL% LSS 8 SET ERRORLEVEL = 0
+(robocopy "Assets" "%modpath%\Assets" /E) ^& IF %ERRORLEVEL% LSS 8 SET ERRORLEVEL = 0
