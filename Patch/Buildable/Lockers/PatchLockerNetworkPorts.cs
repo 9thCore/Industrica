b2929f53e75c810ff4a44d19d2e708e387caabda
9thCore <38700349+9thCore@users.noreply.github.com>
2025-07-15 20:04:26 +0300

    Add ports to wall locker & normal locker

---
+++ Patch/Buildable/Lockers/PatchLockerNetworkPorts.cs
@@ -0,0 +1,86 @@
+﻿using Industrica.Network.Container.Provider;
+using Industrica.Network.Physical;
+using Industrica.Utility;
+using UnityEngine;
+using UWE;
+
+namespace Industrica.Patch.Buildable.Lockers
+{
+    public static class PatchLockerNetworkPorts
+    {
+        public static void PatchAll()
+        {
+            PatchLocker();
+            PatchSmallLocker();
+        }
+
+        private static void PatchLocker()
+        {
+            CoroutineHost.StartCoroutine(PrefabUtil.RunOnPrefab(TechType.Locker, go =>
+            {
+                go.EnsureComponent<StorageContainerProvider>();
+
+                Quaternion rotation = Quaternion.Euler(90f, 0f, 0f);
+                Vector3 front = Vector3.forward * 0.25f;
+                Vector3 topSection = Vector3.up * 1.48f;
+                Vector3 bottomSection = Vector3.up * 0.39f;
+                Vector3 rightSide = Vector3.right * -0.17f;
+                Vector3 leftSide = -rightSide;
+
+                PhysicalNetworkItemPort.CreatePort(
+                    go,
+                    rightSide + topSection + front,
+                    rotation,
+                    Network.PortType.Input,
+                    false);
+
+                PhysicalNetworkItemPort.CreatePort(
+                    go,
+                    leftSide + topSection + front,
+                    rotation,
+                    Network.PortType.Input,
+                    false);
+
+                PhysicalNetworkItemPort.CreatePort(
+                    go,
+                    rightSide + bottomSection + front,
+                    rotation,
+                    Network.PortType.Output,
+                    false);
+
+                PhysicalNetworkItemPort.CreatePort(
+                    go,
+                    leftSide + bottomSection + front,
+                    rotation,
+                    Network.PortType.Output,
+                    false);
+            }));
+        }
+
+        private static void PatchSmallLocker()
+        {
+            CoroutineHost.StartCoroutine(PrefabUtil.RunOnPrefab(TechType.SmallLocker, go =>
+            {
+                go.EnsureComponent<StorageContainerProvider>();
+
+                Vector3 topAndFront = Vector3.up * 0.42f + Vector3.forward * 0.17f;
+                Vector3 rightSide = Vector3.right * -0.42f;
+                Vector3 leftSide = -rightSide;
+
+                PhysicalNetworkItemPort.CreatePort(
+                    go,
+                    leftSide + topAndFront,
+                    Quaternion.Euler(0f, 0f, 270f),
+                    Network.PortType.Input,
+                    false);
+
+                PhysicalNetworkItemPort.CreatePort(
+                    go,
+                    rightSide + topAndFront,
+                    Quaternion.Euler(0f, 0f, 90f),
+                    Network.PortType.Output,
+                    false);
+            }));
+        }
+    }
+}
