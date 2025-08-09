88b196e0458f58d4b9bae10ba5f0c6f484afc52c
9thCore <38700349+9thCore@users.noreply.github.com>
2025-08-09 10:19:42 +0300

    Utility for UI generation

---
+++ UI/SpriteDataFix/BaseSpriteDataFix.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Industrica.UI.SpriteDataFix
+{
+    // Because sprite data is not kept on prefab initialisation...
+    public abstract class BaseSpriteDataFix : MonoBehaviour
+    {
+        public uGUI_Icon icon;
+
+        public abstract Atlas.Sprite Sprite { get; }
+
+        public virtual void PerformAdditionalInit() { }
+
+        public void Start()
+        {
+            icon.sprite = Sprite;
+            GameObject.DestroyImmediate(this);
+        }
+
+        public void Init(Vector2 size)
+        {
+            icon.material = new Material(uGUI_ItemIcon.iconMaterial);
+
+            RectTransformExtensions.Fit(icon.rectTransform, size.x, size.y, Sprite.size.x, Sprite.size.y, false);
+
+            icon.material.SetVector(ShaderPropertyID._Size, size);
+
+            Vector4 vector = new Vector4(-0.5f * size.x, -0.5f * size.y, size.x, size.y);
+            icon.material.SetVector(ShaderPropertyID._FillRect, vector);
+
+            PerformAdditionalInit();
+        }
+    }
+}
---
+++ Utility/UIUtil.cs
@@ -0,0 +1,68 @@
+﻿using FMOD;
+using Industrica.UI;
+using Industrica.UI.SpriteDataFix;
+using System;
+using System.Drawing;
+using TMPro;
+using UnityEngine;
+using static Atlas;
+
+namespace Industrica.Utility
+{
+    public static class UIUtil
+    {
+        public static Canvas CreateCanvas(this GameObject prefab, Vector2? size = null)
+        {
+            Canvas canvas = prefab.CreateChild(nameof(Canvas))
+                .EnsureComponent<Canvas>()
+                .WithScale(x: 0.002f, y: 0.002f);
+
+            RectTransform transform = canvas.GetComponent<RectTransform>();
+
+            if (size != null)
+            {
+                transform.offsetMin = -size.Value / 2f;
+                transform.offsetMax = size.Value / 2f;
+            }
+
+            if (prefab.TryGetComponent(out Constructable _))
+            {
+                prefab.EnsureComponent<ConstructableCanvas>().WithCanvas(canvas);
+            }
+
+            return canvas;
+        }
+
+        public static GameObject CreateUIElement(this Canvas canvas, Vector3? position = null, Quaternion? rotation = null, Vector3? scale = null)
+        {
+            return canvas.gameObject.CreateChild($"UIElement{canvas.transform.childCount}", position: position, rotation: rotation, scale: scale);
+        }
+        
+        public static Canvas WithText(this Canvas canvas, Action<TextMeshProUGUI> callback)
+        {
+            TextMeshProUGUI component = canvas.CreateUIElement().EnsureComponent<TextMeshProUGUI>();
+            callback(component);
+            return canvas;
+        }
+
+        public static Canvas WithBackgroundIcon(this Canvas canvas, CraftData.BackgroundType type, float radius, Vector2 size, Action<uGUI_Icon> callback)
+        {
+            return WithIcon<BackgroundSpriteDataFix>(canvas, size, fixer => fixer.WithType(type).WithRadius(radius), callback);
+        }
+
+        public static Canvas WithIcon<T>(this Canvas canvas, Vector2 size, Action<T> initFixer, Action<uGUI_Icon> callback) where T : BaseSpriteDataFix, new()
+        {
+            canvas.CreateUIElement()
+                .EnsureComponentChained(out T dataFixer)
+                .EnsureComponentChained(out uGUI_Icon icon);
+
+            dataFixer.icon = icon;
+            initFixer(dataFixer);
+            dataFixer.Init(size);
+
+            callback(dataFixer.icon);
+
+            return canvas;
+        }
+    }
+}
