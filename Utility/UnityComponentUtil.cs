21a7cd20bae2c49eca02a7d6bc99b0728986cef1
9thCore <38700349+9thCore@users.noreply.github.com>
2025-08-09 10:18:49 +0300

    Generify TransformUtil

---
+++ Utility/UnityComponentUtil.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace Industrica.Utility
+{
+    public static class UnityComponentUtil
+    {
+        public static T WithScale<T>(this T component, float? x = null, float? y = null, float? z = null) where T : Component
+        {
+            Vector3 scale = component.transform.localScale;
+            if (x.HasValue)
+            {
+                scale.x = x.Value;
+            }
+            if (y.HasValue)
+            {
+                scale.y = y.Value;
+            }
+            if (z.HasValue)
+            {
+                scale.z = z.Value;
+            }
+
+            component.transform.localScale = scale;
+            return component;
+        }
+
+        public static T WithLocalRotation<T>(this T component, Quaternion rotation) where T : Component
+        {
+            component.transform.localRotation = rotation;
+            return component;
+        }
+
+        public static T WithLocalPosition<T>(this T component, Vector3 position) where T : Component
+        {
+            component.transform.localPosition = position;
+            return component;
+        }
+    }
+}
