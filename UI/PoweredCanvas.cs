a301b1e034f3229d31740901a203ee56e5e9c9a6
9thCore <38700349+9thCore@users.noreply.github.com>
2025-08-11 11:06:58 +0300

    Split canvas power update code to new component

---
+++ UI/PoweredCanvas.cs
@@ -0,0 +1,26 @@
+﻿using Industrica.ClassBase;
+using UnityEngine;
+
+namespace Industrica.UI
+{
+    public class PoweredCanvas : BaseMachine, IRelayPowerChangeListener
+    {
+        public Canvas canvas;
+
+        public PoweredCanvas WithCanvas(Canvas canvas)
+        {
+            this.canvas = canvas;
+            return this;
+        }
+
+        public void PowerDownEvent(PowerRelay relay)
+        {
+            canvas.gameObject.SetActive(false);
+        }
+
+        public void PowerUpEvent(PowerRelay relay)
+        {
+            canvas.gameObject.SetActive(true);
+        }
+    }
+}
