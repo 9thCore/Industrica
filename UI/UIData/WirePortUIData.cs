12066d7aba9ee5048e103bc61780deeed554bf59
9thCore <38700349+9thCore@users.noreply.github.com>
2025-08-11 21:09:14 +0300

    Bugfix: wire port UI was not being updated

--- UI/UIData/WirePortUIData.cs
+++ UI/UIData/WirePortUIData.cs
@@ -10,17 +10,21 @@ namespace Industrica.UI.UIData
         public WirePortUIData WithPort(WirePort port)
         {
             this.port = port;
-            port.OnCharge += Update;
             return this;
         }
 
         public override void SetText(TextMeshProUGUI text)
         {
             base.SetText(text);
-            Update();
+            OnCharge();
         }
 
-        public void Update()
+        public void Start()
+        {
+            port.OnCharge += OnCharge;
+        }
+
+        public void OnCharge()
         {
             text.text = port.value.ToString("D2");
         }
