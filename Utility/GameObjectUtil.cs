9e5ee7c648a03e8b6c973626a47333c4fc1bae81
9thCore <38700349+9thCore@users.noreply.github.com>
2025-08-31 14:43:15 +0300

    remove TryFindObjectWith
    bad

--- Utility/GameObjectUtil.cs
+++ Utility/GameObjectUtil.cs
@@ -48,40 +48,6 @@ namespace Industrica.Utility
             toAction.ForEach(action);
         }
 
-        public static bool TryFindObjectWith<T>(Func<T, bool> filter, out T result) where T : MonoBehaviour
-        {
-            result = null;
-
-            T[] allObjects = Resources.FindObjectsOfTypeAll<T>();
-            if (allObjects.Count() == 0)
-            {
-                return false;
-            }
-
-            bool foundMore = false;
-
-            foreach (T obj in allObjects)
-            {
-                if (filter.Invoke(obj))
-                {
-                    if (result == null)
-                    {
-                        result = obj;
-                    } else
-                    {
-                        foundMore = true;
-                    }
-                }
-            }
-
-            if (foundMore)
-            {
-                Plugin.Logger.LogWarning($"Filter wasn't specific enough, more than one object was found. Returning with the first object found");
-            }
-
-            return true;
-        }
-
         public static void SetupConstructableBounds(this GameObject prefab, Vector3? offset = null)
         {
             offset ??= Vector3.zero;
