6d8bd159bf88aa7acc0aca59c31a23215f466dfe
9thCore <38700349+9thCore@users.noreply.github.com>
2025-09-21 19:53:51 +0300

    Use "Sprite" subdirectory for... sprites

--- Utility/PathUtil.cs
+++ Utility/PathUtil.cs
@@ -9,11 +9,12 @@ namespace Industrica.Utility
     {
         public static readonly string AssemblyPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
         public static readonly string AssetPath = Path.Combine(AssemblyPath, "Assets");
+        public static readonly string SpritePath = Path.Combine(AssetPath, "Sprite");
         public static readonly string TexturePath = Path.Combine(AssetPath, "Texture");
 
         public static Sprite GetImage(string path)
         {
-            string fullPath = Path.Combine(AssetPath, $"{path}.png");
+            string fullPath = Path.Combine(SpritePath, $"{path}.png");
             if (!File.Exists(fullPath))
             {
                 throw new FileNotFoundException();
